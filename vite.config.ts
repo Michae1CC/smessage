80cbefd1283a98fea664482a4b98957770e98b1d
Michael CC <michael.ciccotostocamp@deswik.com>
2023-08-31 10:07:06 +1000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+  },
+  plugins: [react()],
+})
