54522af94010dee9a1f70ff3780964a5c13a9764
Michael CC <333mmcc@gmail.com>
2023-09-02 19:11:09 +1000

    Added main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.scss'
+import "./App.scss";
 
 function App() {
   // https://getbootstrap.com/docs/4.3/utilities/overflow/
   // https://bootsnipp.com/snippets/6XlB5
   return (
     <>
-      <div className="container-fluid" style={{backgroundColor: "blue"}}>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div className="container-fluid outer">
+        <div className="row justify-content-center h-100">
+          <div className="h-100 d-flex flex-column">
+            <div className="row justify-content-center flex-grow-1 main-display">
+              <div className="text-white">ROW 2 - grow remaining height</div>
+            </div>
+            <div className="row justify-content-center main-input">
+              <div className="input-group" style={{ padding: "2px" }}>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Recipient's username"
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                />
+                <div className="input-group-append">
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    Button
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import {resolve} from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
-    }
+      "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
+    },
   },
   server: {
     port: 8080,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
