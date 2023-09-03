a2d76bf1340bc1506d9957a66026aa112e382849
Michael CC <michael.ciccotostocamp@deswik.com>
2023-09-04 08:19:01 +1000

    Added user messages

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,35 @@ function App() {
         <div className="row justify-content-center h-100">
           <div className="h-100 d-flex flex-column">
             <div className="row justify-content-center flex-grow-1 main-display">
-              <div className="text-white">ROW 2 - grow remaining height</div>
+              <div className="text-white">
+                {/* Text Input here */}
+                <div className="row justify-content-md-end">
+                  <div className="col-md-auto text-user">
+                    Lorem ipsum dolor sit amet.
+                  </div>
+                </div>
+                {/* Text Input here */}
+                <div className="row justify-content-md-end">
+                  <div className="col-md-auto text-user">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Praesent sed vehicula nisi, sed feugiat justo. Etiam a sem
+                    justo. Nam varius egestas ipsum, sed sodales ex sagittis
+                    quis. Suspendisse varius mi eu dapibus egestas. Vivamus
+                    purus enim, tempus sit amet odio et, suscipit imperdiet sem.
+                    Aenean nisl orci, pharetra ut bibendum ut, faucibus eget
+                    tortor. Pellentesque habitant morbi tristique senectus et
+                    netus et malesuada fames ac turpis egestas. Praesent a nunc
+                    id eros gravida consequat. Aenean nibh lorem, venenatis non
+                    enim eu, imperdiet rutrum tellus. Nulla consectetur placerat
+                    erat, a fermentum ex bibendum a. Suspendisse id iaculis
+                    augue. Integer ante mi, pulvinar et lectus ornare, gravida
+                    consectetur orci. Duis imperdiet elit eu dui suscipit
+                    sodales. Quisque fringilla rhoncus elementum. Donec placerat
+                    vel nulla vitae congue. Aenean faucibus neque tellus,
+                    commodo pharetra arcu elementum eget.
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="row justify-content-center main-input">
               <div className="input-group" style={{ padding: "2px" }}>
